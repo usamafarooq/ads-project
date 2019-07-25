a23333e2694b23f6127718957f08730a288598f4
m0zee <moiz.hanif786@gmail.com>
2019-07-26 03:11:49 +0500

    Update view.php

--- application/views/ads/view.php
+++ application/views/ads/view.php
@@ -12,12 +12,19 @@
 	<div class="row">
 		<div class="col-lg-12 col-md-12 col-xs-12 page-content">
 
-
+			<?php if (!empty($ads['image'])): ?>
+				
+			<div style="text-align: center">
+				<img src="<?php echo base_url($ads['image']) ?>" alt="" width="">
+			</div>
+			<?php else: ?>
 				<?php if (strpos($ads['link'], 'http') === FALSE): ?>
 					<?php $ads['link'] = 'http://'.$ads['link'] ?>
 
 				<?php endif ?>
 			    <iframe src="<?php echo $ads['link'] ?>" frameborder="0" style="overflow:hidden;height: 100vh !important;width:100%" height="100%" width="100%"></iframe>
+			<?php endif ?>
+
 		</div>
 	</div>
 </div>
